af91a4c9140a529c7f31b401e7d1d393a02ee1bb
AHTOH2001 <antonlapytko@gmail.com>
2020-05-24 09:08:40 +0300

    Initial commit

---
+++ Cshlab8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cshlab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
